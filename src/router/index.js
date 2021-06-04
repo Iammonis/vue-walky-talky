f5a060ef1a1d9b9578c3c85206d7b641883f01e8
Monis Khan <immoniskhan@gmail.com>
2021-06-05 03:58:21 +0530

    Added dynamic routes for chats

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,13 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../components/Home';
+import ChatRoom from '../components/ChatRoom';
 
 Vue.use(VueRouter)
 
 const routes = [
-    { path: '/', component: Home }
+    { path: '/', component: Home },
+    { path: '/chats/:id', component: ChatRoom, name: 'chat' }
 ];
 
 const router = new VueRouter({
